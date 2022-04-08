3620518d48f618c7eac6c89271b6381563b0e6ad
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-08 15:08:32 +0200

    lte26007: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: xiaomi Model: HM 2A Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_lte26007.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_lte26007-user \
+    omni_lte26007-userdebug \
+    omni_lte26007-eng
